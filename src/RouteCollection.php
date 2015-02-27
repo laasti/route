0f06290e5fd37a418642f735421a0cede73f2620
Sonia Marquette <contact@nebulousweb.com>
2015-02-26 22:53:10 -0500

    Initial commit

---
+++ src/RouteCollection.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Laasti\Route;
+
+use League\Route\RouteCollection as BaseRouteCollection;
+use Laasti\Services\RouteCollectionInterface;
+/**
+ * Description of RouteCollection
+ *
+ * @author Sonia
+ */
+class RouteCollection extends BaseRouteCollection implements RouteCollectionInterface
+{
+    //put your code here
+}
