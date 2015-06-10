1c771312deffee5670bde6b4dcb77b8b76c537c7
nebulousGirl <contact@nebulousweb.com>
2015-06-10 16:54:41 -0400

    Removed mount, it is not needed

--- src/RouteCollector.php
+++ src/RouteCollector.php
@@ -22,37 +22,28 @@ class RouteCollector
 
     protected $container;
     protected $routes;
-    protected $mount;
 
-    public function __construct($mount, RouteCollection $routes, ContainerInterface $container)
+    public function __construct(RouteCollection $routes, ContainerInterface $container)
     {
-        $this->mount = $mount;
         $this->container = $container;
         $this->routes = $routes;
     }
 
-    public function setMount($mount)
+    public function add($route)
     {
-        $this->mount = $mount;
-        return $this;
-    }
-
-    public function add($route, $mount = null)
-    {
-        $mount = is_null($mount) ? $this->mount : $mount;
 
         if (is_string($route)) {
             $classname = $route;
             $route = $this->container->get($route);
             $this->container->add($classname, $route);
         } else {
-            $classname = 'Router_' . $route->getMethod() . '' . $mount . $route->getUri();
+            $classname = 'Router_' . $route->getMethod() . '' . $route->getUri();
             $this->container->add($classname, $route);
         }
 
 
         if ($route instanceof RouteInterface) {
-            $this->routes->addRoute($route->getMethod(), $mount . $route->getUri(), $classname . '::boot');
+            $this->routes->addRoute($route->getMethod(), $route->getUri(), $classname . '::boot');
             return $route;
         }
 
